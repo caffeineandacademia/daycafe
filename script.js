4f71ea14ada718edf992486e9b338938ea9d69ef
jessicaranieses <143584120+jessicaranieses@users.noreply.github.com>
2023-09-08 17:32:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let attempts = 5;
+let actualCustomers;
+
+const BASE_CUSTOMERS = 100;
+const RATIO = 0.1;
+
+function initialize() {
+    document.getElementById("twoMonthsAgo").textContent = "350";
+    document.getElementById("lastMonth").textContent = "400";
+    document.getElementById("residents").textContent = "3000";
+}
+
+function generateActualCustomers() {
+    // Generate a random number of customers around our prediction
+    let residents = parseInt(document.getElementById("residents").textContent);
+    let expectedCustomers = BASE_CUSTOMERS + RATIO * residents;
+    return Math.floor(expectedCustomers + (Math.random() * 50 - 25)); // +/- 25 variance for unpredictability
+}
+
 function checkPrediction() {
     if (attempts > 0) {
         let predictedCustomers = parseInt(document.getElementById("predictedCustomers").value);
@@ -15,7 +34,7 @@ function checkPrediction() {
         feedbackMessage += "The actual customers were " + actualCustomers + ".";
         document.getElementById("feedback").textContent = feedbackMessage;
 
-        // Adding attempt history
+        // Adding attempt history to the table
         let historyTableBody = document.getElementById("historyTableBody");
         let newRow = historyTableBody.insertRow();
         newRow.insertCell().textContent = 5 - attempts + 1; // Attempt Number
